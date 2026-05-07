9130fd2674693ce50f454aa9921ddbc5283fa50b
Tvbaal <cristo.perezr@duocuc.cl>
2026-05-07 18:49:05 -0400

    creacion de publicacion real

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react' // 1. Importamos React y useState
 import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { SideBar } from './components/SideBar/SideBar'
@@ -6,12 +7,16 @@ import { StoryViewPage } from './pages/StoryViewPage'
 import Footer from './components/Footer'
 import Terms from './pages/Terms'
 import Privacy from './pages/Privacy'
+import CreatePostModal from './components/PostCreate/CreatePostModal' 
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   return (
-    // Quitamos bg-black y text-white, agregamos un color de fondo verde muy suave
     <div className="d-flex min-vh-100" style={{ backgroundColor: '#f4f9f4', color: '#333' }}>
-      <SideBar />
+      
+      <SideBar onOpenModal={() => setIsModalOpen(true)} />
+      
       <div className="main-area flex-grow-1 d-flex flex-column">
         <div className="flex-grow-1">
           <Routes>
@@ -24,6 +29,8 @@ function App() {
         </div>
         <Footer />
       </div>
+      
+      <CreatePostModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
       
     </div>
   )
