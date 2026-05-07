a55a744f6b8bff532686b4d8f80d10b2a826d074
NicoAranda <n.aranda@duocuc.cl>
2026-05-07 16:16:05 -0400

    arreglo de conflictos

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
