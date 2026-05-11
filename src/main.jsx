03c9ff8298047bd5a53cea7f33b0d1cf11407cd2
NicoAranda <n.aranda@duocuc.cl>
2026-05-10 22:39:36 -0400

    error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@mdi/font/css/materialdesignicons.min.css';
 import './index.css'
 import './App.css'
